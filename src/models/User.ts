9f897079d0d66e0b974ced9c27957b3dc288e490
manziro785 <manziro785@gmail.com>
2025-11-14 11:45:59 +0600

    added changes and optimized

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,7 +12,7 @@ User.init(
   {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    nickname: { type: DataTypes.STRING, allowNull: false },
+    nickname: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
   },
   { sequelize, modelName: "User" }
